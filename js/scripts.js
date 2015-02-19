cfff2283d4395e095aa9c643c0567216a33f4f4d
Frank Meza and Matthew Terrill <fmeza07@gmail.com>
2015-02-19 09:56:16 -0800

    update html and js for additional addresses

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,44 @@
 $(function() {
-  $("form#address-book").submit(function(event){
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+    '<div class="form-group">' +
+    '<label for="new-street">Street</label>' +
+    '<input type="text" class="form-control new-street">' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="new-city">City</label>' +
+    '<input type="text" class="form-control new-city">' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="new-state">State</label>' +
+    '<input type="text" class="form-control new-state">' +
+    '</div>' +
+    '</div>');
+  });
+
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedStreet = $("input#new-street").val();
-    var inputtedCity = $("input#new-city").val();
-    var inputtedState = $("input#new-state").val();
-    var inputtedZip = $("input#new-zip").val();
+    var inputtedAddress = $("input#new-address").val();
+
+    var newContact = { firstName: inputtedFirstName,
+                       lastName: inputtedLastName,
+                       address: inputtedAddress };
+
+    $("ul#contacts").append("<li><span class='contact'>" +
+                              newContact.firstName +
+                              " " + newContact.lastName +
+                              "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
 
     var newAddress = { street: inputtedStreet,
                        city: inputtedCity,
@@ -16,19 +49,8 @@ $(function() {
                        }
                       };
 
-    var newContact = { firstName: inputtedFirstName,
-                       lastName: inputtedLastName,
-                       address: newAddress.fullAddress()
-                      };
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-street").val("");
-    $("input#new-city").val("");
-    $("input#new-state").val("");
-    $("input#new-zip").val("");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
@@ -38,6 +60,5 @@ $(function() {
       $(".address").text(newContact.address);
     });
 
-    event.preventDefault();
   });
 });
